13cb2d90cb52e31a6ec583c1bea60a1db6407011
joody <joody@hubify.com.br>
2019-04-11 20:27:15 -0300

    Added some tests for Users class
    Added some tests for the behaviour of the Users Class

---
+++ tests/unit/UsersTest.js
@@ -0,0 +1,18 @@
+describe('Users', () => {
+  // xit for ignore tests for now
+  xit('is an ActiveOrm class', () => {
+
+  });
+
+  describe('find()', () => {
+    xit('returns a single result', () => {
+
+    });
+
+    context('when it does not have records', () => {
+      xit('returns empty', () => {
+
+      });
+    });
+  });
+});
